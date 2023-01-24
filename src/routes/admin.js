d98874394d7e7acc05aeb34e1f0c0741cf8c498f
Halldór Hrafn Reynisson <halldor.hrafn.reynisson@gmail.com>
2023-01-24 08:47:13 +0000

    Some minor changes to the chat app and admin site + added a new branch

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,17 +3,19 @@ require('dotenv/config');
 
 const router = express.Router();
 
+const isLoggedIn = require('../middleware/isLoggedIn');
+
 router.get('/', (req, res) => {
   if (req.session.admin || process.env.DEV) {
     const title = 'Admin page';
 
     let user = isLoggedIn(req.session);
 
-  if (!user) {
-    user.isLoggedIn = false;
-  }
+    if (!user) {
+      user.isLoggedIn = false;
+    }
 
-    res.render('admin', { title, isLoggedIn, username, admin})
+    res.render('admin', { title, user })
   } else {
     res.redirect('/');
   }
